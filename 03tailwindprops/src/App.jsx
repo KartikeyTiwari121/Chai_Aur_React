d5e8ca1e1ed9d3e015a806e350813b970c65dea3
KartikeyTiwari121 <htnimcet121@gmail.com>
2024-02-16 13:44:05 +0530

    Learned about tailwind and props in react js, and how we can make a components and use multiple times, and at using part how we can pass props to components and handle it.

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/card'
+
+function App() {
+  const [count, setCount] = useState(0)
+  let mObj = {
+    myName:"harsh",
+    age: 20,
+    myArr:[1,2,3]
+  } 
+
+  return (
+    <>
+    <h1 className='bg-green-500 text-black p-4 rounded-2xl mb-4'>Tailwind Test</h1> 
+    <Card myName="Harsh" btnText="click me"/>
+    <Card myName="Putin" btnText="touch me"/>
+
+    </>
+  )
+}
+
+export default App
